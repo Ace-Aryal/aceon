8ec7c5a4851f17d8175c5a78873b75a95db65ee4
Dipesh Aryal <aryaldeelep@gmail.com>
2025-08-02 09:41:12 +0545

    Schaffhold new project with nativewind

--- aceon/app/index.tsx
+++
@@ -1,9 +0,0 @@
-import { Text, View } from "react-native";
-
-export default function Index() {
-  return (
-    <View className="flex justify-center items-center text-red-500">
-      <Text>Hello</Text>
-    </View>
-  );
-}
