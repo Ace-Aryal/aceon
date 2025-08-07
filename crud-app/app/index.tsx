f0fb5bf28137adc91afe0368aad9bf9c73970102
Dipesh Aryal <aryaldeelep@gmail.com>
2025-08-07 08:59:32 +0545

    Learn about react-native-reanimated, fonts and theme toggling and asyncStorage

--- crud-app/app/index.tsx
+++ crud-app/app/index.tsx
@@ -1,5 +1,5 @@
 import { cn } from "@/lib/utils";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import {
   Appearance,
   FlatList,
@@ -10,12 +10,20 @@ import {
   View,
 } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
+import { useFonts, Inter_500Medium } from "@expo-google-fonts/inter";
+import Animated, { LinearTransition } from "react-native-reanimated";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 export default function Index() {
+  const [loaded, error] = useFonts({
+    Inter_500Medium,
+  });
+  // wait for it to at least render the font
+
   const colorScheme = Appearance.getColorScheme();
   const isDarkMode = colorScheme === "dark";
   const [todos, setTodos] = useState<
     | {
-        id: string;
+        id: number;
         title: string;
         completed: boolean;
       }[]
@@ -26,23 +34,24 @@ export default function Index() {
     setTodos((prev) =>
       prev
         ? [
-            ...prev,
             {
-              id: text + Math.random(),
+              id: new Date().getSeconds(),
               title: text,
               completed: false,
             },
+            ...prev,
           ]
         : [
             {
-              id: text + Math.random(),
+              id: new Date().getSeconds(),
               title: text,
               completed: false,
             },
           ]
     );
+    setText("");
   };
-  const toggleComplete = (id: string) => {
+  const toggleComplete = (id: number) => {
     const newTodos = todos?.map((todo) => {
       if (todo.id === id) {
         return { ...todo, completed: !todo.completed };
@@ -50,12 +59,42 @@ export default function Index() {
       return todo;
     });
     setTodos(newTodos ?? null);
-    setText("");
   };
-  const deleteTodo = (id: string) => {
+  const deleteTodo = (id: number) => {
     const newTodos = todos?.filter((todo) => todo.id !== id);
     setTodos(newTodos ?? null);
   };
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const jsonValue = await AsyncStorage.getItem("todoApp");
+        const storageTodos:
+          | {
+              id: number;
+              title: string;
+              completed: boolean;
+            }[]
+          | null = jsonValue !== null ? JSON.parse(jsonValue) : null;
+        if (storageTodos && storageTodos.length) {
+          setTodos(storageTodos.sort((a, b) => a.id - b.id));
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchData();
+  }, []);
+  useEffect(() => {
+    const storeData = async () => {
+      try {
+        const jsonValue = JSON.stringify(todos);
+        AsyncStorage.setItem("todoApp", jsonValue);
+      } catch (error) {}
+    };
+  }, [todos?.length]);
+  if (!loaded && !error) {
+    return null;
+  }
   return (
     <View
       style={{
@@ -65,50 +104,71 @@ export default function Index() {
         "bg-zinc-950": isDarkMode,
       })}
     >
-      <TextInput
-        value={text}
-        onChangeText={setText}
-        className={cn("border border-gray-500  rounded-lg text-zinc-950 ", {
-          "text-gray-50": isDarkMode,
-        })}
-        placeholder="Enter todo...."
-      />
-      <Pressable
-        onPress={() => {
-          addTodo();
-        }}
-        disabled={text.length < 1}
-        className={cn(
-          "self-center mt-2 bg-gray-800 px-4 py-2 rounded-lg active:bg-gray-700 ",
-          {
-            "bg-gray-300 active:bg-gray-200": !isDarkMode,
-          }
-        )}
-      >
-        <Text
-          className={cn("text-zinc-950", {
-            "text-gray-50": isDarkMode,
-          })}
+      <View className="flex-row w-full gap-2">
+        <TextInput
+          style={{
+            fontFamily: "Inter_500Medium",
+          }}
+          value={text}
+          placeholderTextColor={isDarkMode ? "white" : "black"}
+          onChangeText={setText}
+          className={cn(
+            "border border-gray-500 flex-1  rounded-lg text-zinc-950 ",
+            {
+              "text-gray-50": isDarkMode,
+            }
+          )}
+          placeholder="Enter todo...."
+        />
+        <Pressable
+          onPress={() => {
+            addTodo();
+          }}
+          disabled={text.length < 1}
+          className={cn(
+            "self-center mt-2 bg-gray-800 px-4 py-4 w-fit rounded-lg active:bg-gray-700 ",
+            {
+              "bg-gray-300 active:bg-gray-200": !isDarkMode,
+            }
+          )}
         >
-          Add Todo
-        </Text>
-      </Pressable>
+          <Text
+            className={cn("text-zinc-950", {
+              "text-gray-50": isDarkMode,
+            })}
+          >
+            Add
+          </Text>
+        </Pressable>
+      </View>
+
       {todos && todos.length > 0 ? (
-        <View className="flex-1">
-          <FlatList
+        <View className="flex-1 mt-4">
+          <Animated.FlatList
             data={todos}
-            keyExtractor={(todo) => todo.id}
+            keyExtractor={(todo) => todo.id.toString()}
+            itemLayoutAnimation={LinearTransition}
+            keyboardDismissMode="on-drag"
+            ItemSeparatorComponent={() => (
+              <View className="h-2 w-full bg-none" />
+            )}
             renderItem={(todo) => (
               <View
                 onTouchStart={() => toggleComplete(todo.item.id)}
                 className={cn(
-                  "flex flex-row gap-2 flex-1 items-center justify-between"
+                  "flex flex-row bg-gray-100  p-2 rounded-lg flex-1 items-center justify-between",
+                  {
+                    "bg-gray-700": isDarkMode,
+                  }
                 )}
               >
                 <Text
-                  className={cn("text-zinc-950 w-4/5", {
+                  style={{
+                    fontFamily: "Inter_500Medium",
+                  }}
+                  className={cn("text-zinc-950 w-4/5 text-lg", {
                     "text-gray-50": isDarkMode,
-                    "line-through": todo.item.completed,
+                    "line-through text-gray-500": todo.item.completed,
                   })}
                 >
                   {todo.item.title}
@@ -120,7 +180,9 @@ export default function Index() {
                   className="w-1/5 flex flex-row justify-center"
                 >
                   <Ionicons
-                    className="text-red-500 h-5 w-5"
+                    color={"red"}
+                    size={20}
+                    className="text-red-500"
                     name="trash-bin-outline"
                   />
                 </TouchableOpacity>
@@ -130,7 +192,7 @@ export default function Index() {
         </View>
       ) : (
         <Text
-          className={cn("text-zinc-950", {
+          className={cn("text-zinc-950 text-center mt-4", {
             "text-gray-50": isDarkMode,
           })}
         >
